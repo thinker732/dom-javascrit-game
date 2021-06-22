518242f13cf55975b4f9a1f80a94e4b7899f6941
thinker732 <baedsavitar@gmail.com>
2021-06-22 17:38:16 +0100

    select element

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.dir(document);
-document.body.textContent = "Hello";
+//document.body.textContent = "Hello";
+
+const elH1 = document.querySelector("h1");
+
+const allH1 = document.querySelectorAll("h1");
+
+elH1.textContent = "Great";
+
+allH1[1].textContent = "second H1";
